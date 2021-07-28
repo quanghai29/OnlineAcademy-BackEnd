5e4e643a3e69dc568f79fb0ae9d19bb56ec94a09
DungBuiTien1999 <nguyentonhi199@gmail.com>
2021-07-28 20:04:09 +0700

    hehe

--- backend/routes/lecturer/image.route.js
+++ backend/routes/lecturer/image.route.js
@@ -19,48 +19,64 @@ const imageService = require('../../services/image.service');
  *         description: successfully created image
  */
 
- const storage =  multer.diskStorage({
-  filename: async function (req, file, cb) {
-    console.log(req.body);
-    console.log('hehe');
-    // lấy course_id
-    var {course_id, img_title} = req.body;
-
-      //Tạo tên hình ảnh của khóa học
-      var filename = course_id + '.png';
-
-      //Thêm dữ liệu tên ảnh vào csdl
-      var entityImageCourse= {
-          img_title : img_title,
-          img_sourse: `/img/course/${filename}`,
-      };
-
-      const resultImageCourse = await imageService.insertImage(entityImageCourse, course_id);
-      console.log(resultImageCourse);
-
-      cb(null, filename);
+ const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '/public/img/course/')
   },
-  destination: './public/img/course/'
-});
-
-const upload = multer({ storage }).single('courseImage');
-
-router.post('/', async (req, res) => {
-  if (req.files === null) {
-    return res.status(400).json({ msg: 'No file uploaded' });
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now())
   }
-  // console.log(req.body);
-  // console.log(req.files.file.name);
+})
 
-  upload(req, res, (err) => {
-    if(err) {
-      res.status(401).json({message: err.message});
-    }
-    console.log(req);
-    res.json({ fileName: 'add success' });
-  })
+
+const upload = multer({ storage })
+
+router.post('/', upload.single('file'), async (req, res, next) => {
+  console.log(req.file);
+  console.log(req.body);
+  res.status(200).json({message: 'hehe'});
 });
 
 //#endregion
 
 module.exports = router;
+
+// router.post('/', async (req, res) => {
+//   if (req.file === null) {
+//     return res.status(400).json({ msg: 'No file uploaded' });
+//   }
+//   console.log(req.body);
+//   // console.log(req.files.file.name);
+//   console.log(req.file);
+
+//   upload(req, res, (err) => {
+//     if(err) {
+//       res.status(401).json({message: err.message});
+//     }
+//     res.json({ fileName: 'add success' });
+//   })
+// });
+
+// const storage =  multer.diskStorage({
+//   filename: async function (req, file, cb) {
+//     console.log(req.body);
+//     console.log('hehe');
+//     // lấy course_id
+//     var {course_id, img_title} = req.body;
+
+//       //Tạo tên hình ảnh của khóa học
+//       var filename = course_id + '.png';
+
+//       //Thêm dữ liệu tên ảnh vào csdl
+//       var entityImageCourse= {
+//           img_title : img_title,
+//           img_sourse: `/img/course/${filename}`,
+//       };
+
+//       const resultImageCourse = await imageService.insertImage(entityImageCourse, course_id);
+//       console.log(resultImageCourse);
+
+//       cb(null, filename);
+//   },
+//   destination: './public/img/course/'
+// });
--- backend/services/course.service.js
+++ backend/services/course.service.js
@@ -38,6 +38,7 @@ async function insertCourse(course) {
 async function getLatestCourses(amount) {
   let returnModel = {};
   const ret = await courseModel.getLatestCourses(amount);
+
   if (ret == null) {
     returnModel.code = Code.Not_Found;
   } else {
@@ -47,28 +48,15 @@ async function getLatestCourses(amount) {
   return returnModel;
 }
 
-async function getMostViewVideos(amount) {
-  const ret = await courseModel.getMostViewCourses(amount);
-  return ret;
-}
-
 async function getMostViewCourses(amount) {
   let returnModel = {};
-  const ret = await getMostViewVideos(amount);
+  const ret = await courseModel.getMostViewCourses(amount);
 
-  const ret2 = await Promise.all(
-    ret.map(async (item) => {
-      let course = await courseModel.single(item.course_id);
-      course.view_sum = item.sum_view;
-      return course;
-    })
-  );
-
-  if (ret2 == null) {
+  if (ret == null) {
     returnModel.code = Code.Not_Found;
   } else {
     returnModel.code = Code.Success;
-    returnModel.data = ret2;
+    returnModel.data = ret;
   }
   return returnModel;
 }
