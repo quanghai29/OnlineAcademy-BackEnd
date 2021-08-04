032f03d848eb2739be211a26e493fa8fffd63f0b
DungBuiTien1999 <nguyentonhi199@gmail.com>
2021-08-04 22:36:09 +0700

    add some api about chapter

--- backend/models/chapter.models.js
+++ backend/models/chapter.models.js
@@ -4,5 +4,14 @@ const table_name = 'chapter';
 module.exports = {
     add(chapter) {
         return db(table_name).insert(chapter);
+    },
+    getChapterByCourseId(course_id) {
+        return db(table_name).where('course_id', course_id);
+    },
+    getVideoByChapterId(chapter_id) {
+        return db('video').where('chapter_id', chapter_id)
+    },
+    updateChapterbyId(newChapter, id) {
+        return db(table_name).where('id', id).update(newChapter);
     }
 }
